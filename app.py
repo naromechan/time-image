0a4681883a0db6ed4bff8925e7f61502e48705d0
naromechan <narome409@gmail.com>
2026-06-18 01:32:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def generate_image():
 
     x, y = 200, 860
 
-    draw.text((x, y), f"{now}라니……", font=font, fill=(220,215,200))
+    draw.text((x, y), f"{now}라니……", font=font, fill=(222,222,222))
 
     buf = io.BytesIO()
     img.save(buf, format="PNG")
