23aa2291a7947d0f5815e994c0b7b42c391543e2
naromechan <narome409@gmail.com>
2026-06-18 00:49:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def generate_image():
     draw = ImageDraw.Draw(img)
 
     # ✅ 시스템 Arial 폰트 직접 지정
-    font = ImageFont.truetype("font.ttf", 50)
+    font = ImageFont.truetype("font.ttf", 45)
 
     x, y = 200, 850
 
